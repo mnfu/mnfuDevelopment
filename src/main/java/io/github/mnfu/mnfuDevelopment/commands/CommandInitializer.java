77bf5949d0f1a534f1a08d02a953327c0d25bd44
Rob <95727104+mnfu@users.noreply.github.com>
2023-02-15 17:17:31 -0500

    add a command initialization time tracker

--- src/main/java/io/github/mnfu/mnfuDevelopment/commands/CommandInitializer.java
+++ src/main/java/io/github/mnfu/mnfuDevelopment/commands/CommandInitializer.java
@@ -1,8 +1,13 @@
 package io.github.mnfu.mnfuDevelopment.commands;
 
+import org.bukkit.Bukkit;
 public class CommandInitializer {
     public static void initializeCommands() {
+        Bukkit.getLogger().info("[mnfuDevelopment] Command initialization started");
+        long start = System.currentTimeMillis();
         new Feed();
         new Heal();
+        long end = System.currentTimeMillis();
+        Bukkit.getLogger().info("[mnfuDevelopment] Command initialization finished (" + (end-start) + "ms)");
     }
 }
